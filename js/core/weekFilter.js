b0de4cf9f66837043f87edc9c8d532c8f02e03bb
SAURABH MONDAL <saurabh.jopl@gmail.com>
2026-02-02 15:26:41 +0530

    Update weekFilter.js

--- js/core/weekFilter.js
+++ js/core/weekFilter.js
@@ -2,5 +2,8 @@ const weekSelector = document.getElementById("weekSelector");
 
 weekSelector.addEventListener("change", () => {
   APP_STATE.week = weekSelector.value || null;
-  renderActiveRoute();
+
+  if (typeof renderAll === "function") {
+    renderAll();
+  }
 });
